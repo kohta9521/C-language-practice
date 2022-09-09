6d1124125acb3b7b7942818a8f6319e01e74d65d
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-09-09 13:53:51 +0900

    4-7ファイル追加　4-3 boid void　修正

--- section4/4-3.c
+++ section4/4-3.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-int main(boid) {
-    printf("%d", 10 + 3);
+int main(void) {
+    printf("%d\n", 10 + 3);
+    printf("%d\n", 10 - 3);
+    printf("%d\n", 10 * 3);
+    printf("%d\n", 10 / 3);
+    printf("%dn", 10 + 3 * 6 -7);
     return 0;
 }
