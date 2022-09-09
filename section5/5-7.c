d7c77fee457d36af20225655ad3ae610d4b98aaf
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-09-09 16:28:07 +0900

    scanf関数

--- section5/5-7.c
+++ section5/5-7.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("%d", (int) 3.14);
+    printf("%d", (int) (3.14 * 3 * 3));
 
     return 0;
 }
---
+++ section5/5-9.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+    double suuti;
+    scanf("%lf", &suuti);
+    printf("入力された値は%lf", suuti);
+
+    return 0;
+}
