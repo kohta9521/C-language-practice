abd595db3c989d7dc9e62cf6171636b20dd10f0a
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-09-10 00:15:48 +0900

    1-6 make

---
+++ 第1章　まずは慣れよう/1-6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+
+    //変数に整数値を代入して表示
+    // int n;
+    // n = 15;
+    // printf("nの値は%dです。\n", n);
+
+    //変数を整数値で初期化して表示
+
+    int n = 20;
+    printf("nの値は%dです。\n", n);
+
+    return 0;
+}
+
