ce3dd444c542dd31d9e298ef0595438f8531cb2e
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-09-12 16:32:18 +0900

    2-8.c make

---
+++ 第2章　演算と型/2-8.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int         n1, n2;
+    double d1, d2;
+
+    puts("整数と実数を２つずつ入力せよ。");
+    printf("整数n1 :"); scanf("%d", &n1);
+    printf("整数n2 :"); scanf("%d", &n2);
+    printf("整数d1 :"); scanf("%lf", &d1);
+    printf("整数d2 :"); scanf("%lf", &d2);
+
+    printf("5  *  2 = ");
+
+
+//  途中　内容に関して理解ができたので省略
+
+    return 0;
+}
