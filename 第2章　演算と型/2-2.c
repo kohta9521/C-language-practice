51d67540fbb9da109eb99c0ef8c0c7a54c722508
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-09-12 16:00:12 +0900

    2-2.c make

---
+++ 第2章　演算と型/2-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    int n;
+    printf("nの値を入力せよ: ");
+    scanf("%d", &n);
+
+    printf("n / 5は%dです。\n", n / 5);
+    printf("n %% 5は%dです。\n", n % 5);
+
+    return 0;
+}
