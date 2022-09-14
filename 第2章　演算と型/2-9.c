4d7ed8c49e61b0e8282910c92b4b5095849023b6
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-09-15 00:50:12 +0900

    2-10.c make

---
+++ 第2章　演算と型/2-9.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+
+    double a, b;
+
+    puts("二つの実数を入力せよ。");
+    printf("実数a :"); scanf("%lf", &a);
+    printf("実数b :"); scanf("%lf", &b);
+
+    printf("aの値はbの%f％です。\n, a / b * 100");
+
+    return 0;
+}      
