b61049fdc5e425b95f630bc1e7bfd35d62bfce30
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-09-18 20:55:27 +0900

    宣言と代入を別々に行う

--- section10/10-3.c
+++ section10/10-3.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 int main(void) {
+    //宣言と代入を別々に行う
     int array [10];
-    array [5] = 15;
-    printf("%d\n", array[5]);
-    printf("%d\n", array[2]);
-    printf("%d\n", array[20]);
+    array[5] = 15;
+    printf("%d", array[5]);
 
 
     return 0;
