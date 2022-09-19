86b9a98eabd1f0f256cbcb907e61b3801d77fd51
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-09-19 17:25:55 +0900

    複合文　難しい

--- 第3章　プロフラムの流れの分岐/3-14.c
+++ 第3章　プロフラムの流れの分岐/3-14.c
@@ -2,6 +2,29 @@
 
 int main(void) {
 
+    int a, b;
+
+    puts("二つの整数を入力せよ。");
+    printf("整数A : "); scanf("%d", &a);
+    printf("整数B : "); scanf("%d", &b);
+
+    if (a == b) 
+        puts("二つの値は同じです。");
+    else {
+        int min, max;
+
+        if (a < b) {
+            min = a;
+            max = b;
+        } else {
+            min = b;
+            max = a;
+        }
+
+        printf("小さい方の値は%dです。\n", min);
+        printf("大きい方の値は%dです。\n", max);
+    }
+
 
     return 0;
 }
