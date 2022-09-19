62a97016e6fea297baef2a81e8fcc46bb64cca92
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-09-19 09:55:59 +0900

    sprintf関数 syuruinokotonaru

---
+++ section11/11-8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    char str[20];
+    char str1[] = "Star";
+    char str2[] = "Wars";
+    int i = 3;
+
+    sprintf(str, "%s%s%d", str1,str2,i);
+    printf("%s", str);
+
+    return 0;
+}
