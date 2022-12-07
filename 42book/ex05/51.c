0e8922160b84057dfd5e7b6d655f03f5da7ae04f
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-07 18:19:59 +0900

    add new dir files

--- 42book/ex05/51.c
+++ 42book/ex05/51.c
@@ -6,8 +6,31 @@
 /*   By: kkohta <kkohta@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 18:03:54 by kkohta            #+#    #+#             */
-/*   Updated: 2022/12/07 18:03:56 by kkohta           ###   ########.fr       */
+/*   Updated: 2022/12/07 18:19:39 by kkohta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+int	main(void)
+{
+	printf("あなたの運勢を占います。\n");
+	puts("↓結果↓");
+	srand((unsigned)time(NULL));
+	int fortune = rand() % 4 + 1;
+
+	if (fortune == 1)
+	{
+		printf( "大吉\n");
+	} else if (fortune == 2) {
+		printf(" 中吉\n");
+	} else if (fortune == 3) {
+		printf(" 吉\n");
+	} else {
+		printf(" 凶\n");
+	}
+	puts("今日も一日頑張っていきましょう!!!!");
+	return (0);
+}
