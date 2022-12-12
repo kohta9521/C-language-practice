58271715f0abc58bd3fd9c5aa130bc2ad96fcbdc
k_95.21 <100065508+kohta9521@users.noreply.github.com>
2022-12-12 20:11:24 +0900

    Delete 第1章　まずは慣れよう directory

--- 第1章　まずは慣れよう/1-1.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    printf("%d\n", 15 - 37);
-
-    return 0;
-}
--- 第1章　まずは慣れよう/1-4.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-
-    printf("こんにちは。\nお元気ですか。\n\nさようなら。\n");
-
-    return 0;
-}
