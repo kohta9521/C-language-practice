5fad5d8b203019d86ddf07392107c141b3c9549c
Kohta9521 <kohta0421kochi@gmail.com>
2023-01-19 10:50:20 +0900

    add rep マクロ便利

--- C++/repマクロ/sample2.cpp
+++ C++/repマクロ/sample2.cpp
@@ -1,23 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sample.cpp                                         :+:      :+:    :+:   */
+/*   sample2.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kkohta <kkohta@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/19 10:35:20 by kkohta            #+#    #+#             */
-/*   Updated: 2023/01/19 10:44:01 by kkohta           ###   ########.fr       */
+/*   Created: 2023/01/19 10:44:52 by kkohta            #+#    #+#             */
+/*   Updated: 2023/01/19 10:49:56 by kkohta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i ++)
 
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
 int	main() {
 	rep(i, 3) {
-		cout << "Hello" << endl;
-		cout << "Atcoder" << endl;
+		cout << "Hello rep" << i << endl;
 	}
 }
