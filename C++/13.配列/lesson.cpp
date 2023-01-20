cca1541b731aceb17d45334c2af6c8df658cc05f
Kohta9521 <kohta0421kochi@gmail.com>
2023-01-20 17:01:16 +0900

    error about '{' expecter expression

--- C++/13.配列/lesson.cpp
+++ C++/13.配列/lesson.cpp
@@ -6,11 +6,13 @@
 /*   By: kkohta <kkohta@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 16:53:55 by kkohta            #+#    #+#             */
-/*   Updated: 2023/01/20 16:55:30 by kkohta           ###   ########.fr       */
+/*   Updated: 2023/01/20 17:00:51 by kkohta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
 int	main() {
@@ -19,4 +21,9 @@ int	main() {
 	str = "abcd";
 	cout << str.at(0) << endl;
 	cout << str.size() << endl;
+
+	vector<int> vec;
+	vec = { 25, 100, 64 };
+	cout << vec.at(0) << endl;
+	cout << vec.size() << endl;
 }
