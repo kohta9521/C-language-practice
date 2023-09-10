94894fc88316df6b44380d740dbe590b03f00b1a
Kohta9521 <kohta0421kochi@gmail.com>
2023-09-10 20:43:18 +0900

    add sample

--- Cpp＿Introduction/1.基本文法/11.for/sample.cpp
+++ Cpp＿Introduction/1.基本文法/11.for/sample.cpp
@@ -3,4 +3,37 @@ using namespace std;
 
 int main()
 {
+  int N, A;
+  cin >> N >> A;
+
+  // program
+  for (int i = 0; i < N; i++)
+  {
+    int x;
+    string op;
+    cin >> x >> op;
+
+    if (op == "x")
+    {
+      A += x;
+    }
+    else if (op == "-")
+    {
+      A -= x;
+    }
+    else if (op == "*")
+    {
+      A *= x;
+    }
+    else if (op == "/" && != 0)
+    {
+      A /= x;
+    }
+    else
+    {
+      cout << "error" << endl;
+      break;
+    }
+    cout << i + 1 << ":" << A << endl;
+  }
 }
