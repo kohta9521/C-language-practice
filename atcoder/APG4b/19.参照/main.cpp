305cd680d379d8a940f8e951fcecd09fb4b40c9f
k_95.21 <kohta0421kochi@gmail.com>
2024-06-13 13:41:24 +0900

    add new sansyou

--- atcoder/APG4b/19.参照/main.cpp
+++ atcoder/APG4b/19.参照/main.cpp
@@ -2,8 +2,19 @@
 using namespace std;
 
 // 参照私を用いて、呼び出し側の変数の値を変更する
-void saiten() {
+void saiten(vector<vector<int>> &A, int &correct_count, int &wrong_count) {
     // ここにプログラムを追記
+    for (int i = 0; i < 9; i ++) {
+        for (int j = 0; j < 9; j ++) {
+            if (A.at(i).at(j) != (i + 1) * (j + 1)) {
+                A.at(i).at(j) = (i + 1) * (j + 1);
+                wrong_count++;
+            }
+            else {
+                correct_count++;
+            }
+        }
+    }
 }
 
 
